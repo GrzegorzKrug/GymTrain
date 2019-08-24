c17304b824d254e7c3d20ea508cd59614d92e740
Grzegorz Krug <50370928+GrzegorzKrug@users.noreply.github.com>
2019-08-25 00:36:13 +0200

    update

--- src/GymCartPole.py
+++ src/GymCartPole.py
@@ -10,12 +10,12 @@ env = gym.make('CartPole-v0')
 
 for i_episode in range(5):
     observation = env.reset()
-    for t in range(200):
+    for t in range(2000):
         env.render()
 ##        print(observation)
         action = env.action_space.sample()
         observation, reward, done, info = env.step(action)
-
+        # print(reward)
         if done:
             print("Episode finished after {} timesteps".format(t+1))
             break
