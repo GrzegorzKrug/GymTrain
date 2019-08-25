d05378905ea1f9e72917edf08f0a6a8b282a680a
GrzegorzKrug <50370928+GrzegorzKrug@users.noreply.github.com>
2019-08-25 17:21:02 +0200

    Update tf_tutorial.py

--- src/tf_tutorial.py
+++ src/tf_tutorial.py
@@ -1,23 +1,77 @@
 import tensorflow as tf
+import random
 from tensorflow import keras
 import numpy as np
 
-fashion_mnist = keras.datasets.fashion_mnist
-(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+# fashion_mnist = keras.datasets.fashion_mnist
+# (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+# 
+# model = keras.Sequential([
+#     keras.layers.Flatten(input_shape=(28, 28)),
+#     keras.layers.Dense(128, activation=tf.nn.relu),
+#     keras.layers.Dense(10, activation=tf.nn.softmax)
+# ])
+#
+# model.compile(optimizer='adam',
+#               loss='sparse_categorical_crossentropy',
+#               metrics=['accuracy'])
+#
+#
+# if __name__ == '__main__':
+#     model.fit(train_images, train_labels, epochs=5)
+#     test_loss, test_acc = model.evaluate(test_images, test_labels)
+#     print('Test accuracy: {}'.format(test_acc))
 
 
-model = keras.Sequential([
-    keras.layers.Flatten(input_shape=(28, 28)),
-    keras.layers.Dense(128, activation=tf.nn.relu),
-    keras.layers.Dense(10, activation=tf.nn.softmax)
-])
+class Model:
+    def __init__(self, num_states, num_actions, batch_size):
+        self._num_states = num_states
+        self._num_actions = num_actions
+        self._batch_size = batch_size
+        # define the placeholders
+        self._states = None
+        self._actions = None
+        # the output operations
+        self._logits = None
+        self._optimizer = None
+        self._var_init = None
+        # now setup the model
+        self._define_model()
 
-model.compile(optimizer='adam',
-              loss='sparse_categorical_crossentropy',
-              metrics=['accuracy'])
+    def _define_model(self):
+        self._states = tf.placeholder(shape=[None, self._num_states], dtype=tf.float32)
+        self._q_s_a = tf.placeholder(shape=[None, self._num_actions], dtype=tf.float32)
+        # create a couple of fully connected hidden layers
+        fc1 = tf.layers.dense(self._states, 50, activation=tf.nn.relu)
+        fc2 = tf.layers.dense(fc1, 50, activation=tf.nn.relu)
+        self._logits = tf.layers.dense(fc2, self._num_actions)
+        loss = tf.losses.mean_squared_error(self._q_s_a, self._logits)
+        self._optimizer = tf.train.AdamOptimizer().minimize(loss)
+        self._var_init = tf.global_variables_initializer()
 
 
+    def predict_one(self, state, sess):
+        return sess.run(self._logits, feed_dict={self._states: state.reshape(1, self._num_states)})
 
-model.fit(train_images, train_labels, epochs=5)
-test_loss, test_acc = model.evaluate(test_images, test_labels)
-print('Test accuracy: {}'.format(test_acc))
+    def predict_batch(self, states, sess):
+        return sess.run(self._logits, feed_dict={self._states: states})
+
+    def train_batch(self, sess, x_batch, y_batch):
+        sess.run(self._optimizer, feed_dict={self._states: x_batch, self._q_s_a: y_batch})
+
+
+class Memory:
+    def __init__(self, max_memory):
+        self._max_memory = max_memory
+        self._samples = []
+
+    def add_sample(self, sample):
+        self._samples += [sample]
+        if len(self._samples) > self._max_memory:
+            self._samples.pop(0)
+
+    def sample(self, no_samples):
+        if no_samples > len(self._samples):
+            return random.sample(self._samples, len(self._samples))
+        else:
+            return random.sample(self._samples, no_samples)
