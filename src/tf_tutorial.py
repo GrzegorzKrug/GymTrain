8bf30a2e2bd82386cd21254c94a8a1ae52ac4f53
Grzegorz Krug <grzesiekk321@gmail.com>
2019-09-16 12:42:56 +0200

    backup

--- src/tf_tutorial.py
+++ src/tf_tutorial.py
@@ -31,19 +31,23 @@ class NNModel:
         self._num_states = num_states
         self._num_actions = num_actions
         self._batch_size = batch_size
+        
         # define the placeholders
         self._states = None
         self._actions = None
         # the output operations
+        
         self._logits = None
         self._optimizer = None
         self._var_init = None
+        
         # now setup the model
         self._define_model()
 
     def _define_model(self):
         self._states = tf.placeholder(shape=[None, self._num_states], dtype=tf.float32)
         self._q_s_a = tf.placeholder(shape=[None, self._num_actions], dtype=tf.float32)
+
         # create a couple of fully connected hidden layers
         fc1 = tf.layers.dense(self._states, 50, activation=tf.nn.relu)
         fc2 = tf.layers.dense(fc1, 50, activation=tf.nn.relu)
