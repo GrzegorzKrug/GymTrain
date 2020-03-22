edbd0da6556768cd44caca40612ede8f860380d3
Grzegorz Krug <grzesiekk321@gmail.com>
2020-03-22 22:53:06 +0100

    blank cart

--- gym-train/AICartDriver.py
+++ gym-train/AICartDriver.py
@@ -1,15 +1,18 @@
 import gym
 import numpy as np
 import random
-# import tensorflow as tf
-
+import time
 
 env = gym.make('CartPole-v1')
-env.reset()
-for _ in range(1000):
-	env.render()
-	# next_action = env.action_space.sample()
-	# print(next_action)
-	env.step(env.action_space.sample())
-
+for i_episode in range(20):
+    observation = env.reset()
+    for t in range(100):
+        env.render()
+        # print(observation)
+        action = env.action_space.sample()
+        observation, reward, done, info = env.step(action)
+        time.sleep(0.01)
+        if done:
+            print("Episode finished after {} timesteps".format(t+1))
+            break
 env.close()
