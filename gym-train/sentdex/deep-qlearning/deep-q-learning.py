e99992290d0b1bc5f46d9c8458809aa888aa1eeb
Grzegorz Krug <grzesiekk321@gmail.com>
2020-04-16 21:44:25 +0200

    Training in parallel

--- gym-train/sentdex/deep-qlearning/deep-q-learning.py
+++ gym-train/sentdex/deep-qlearning/deep-q-learning.py
@@ -19,18 +19,21 @@ config.gpu_options.allow_growth = True
 config.gpu_options.per_process_gpu_memory_fraction = 0.3
 sess = tf.compat.v1.Session(config=config)
 
-REPLAY_MEMORY_SIZE = 5 * 200
-MIN_REPLAY_MEMORY_SIZE = 5 * 200
-MINIBATCH_SIZE = 256
+REPLAY_MEMORY_SIZE = 5 * 10 * 200
+MIN_REPLAY_MEMORY_SIZE = 10 * 200
+
+MINIBATCH_SIZE = 1000
 
 SHOW_EVERY = 50
 TRAIN_EVERY = 5
 
 EPOCHS = 500
-INITIAL_EPS = 0.8
-END_EPS = -0.1
+INITIAL_EPS = 0.6
+END_EPS = 0.1
 EPS_END_AT = 50
 DISCOUNT = 0.99
+
+SIM_COUNT = 10
 
 MODEL_NAME = "256x256"
 
@@ -93,15 +96,15 @@ class DQNAgent:
     def create_model(self):
         model = Sequential([
                 Flatten(input_shape=self.observation_space_vals),
-                Dense(16, activation='relu'),
-                Dropout(0.2),
+                Dense(32, activation='linear'),
+                # Dropout(0.2),
 
                 # Flatten(),
 
-                Dense(16, activation='relu'),
+                Dense(8, activation='linear'),
                 Dense(self.action_space_size, activation='linear')
         ])
-        model.compile(optimizer=Adam(lr=0.001),
+        model.compile(optimizer=Adam(lr=0.01),
                       loss="mse",
                       metrics=['accuracy'])
 
@@ -131,9 +134,9 @@ class DQNAgent:
 
         # mini batch:
         # (array([-0.49398561,  0.00714135]), 1, array([-0.48706607,  0.00691954]), -1.0, False)
-        minibatch = random.sample(self.replay_memory, self.mini_batch_size)
-        old_states = np.array([transition[0] for transition in minibatch])
-        new_states = np.array([transition[2] for transition in minibatch])
+        memory = random.sample(self.replay_memory, self.mini_batch_size)
+        old_states = np.array([transition[0] for transition in memory])
+        new_states = np.array([transition[2] for transition in memory])
 
         current_qs_list = self.model.predict(old_states)
         future_qs_list = self.target_model.predict(new_states)
@@ -141,10 +144,11 @@ class DQNAgent:
         X = []
         y = []
 
-        for index, (old_state, action, new_state, reward, done) in enumerate(minibatch):
+        for index, (old_state, action, new_state, reward, done) in enumerate(memory):
             if not done:
                 max_future_q = np.max(future_qs_list[index])
                 new_q = reward + DISCOUNT * max_future_q
+
             else:
                 new_q = reward
 
@@ -156,12 +160,13 @@ class DQNAgent:
 
         X = np.array(X)
         y = np.array(y)
-        self.model.fit(
+        history = self.model.fit(
                 X, y,
-                verbose=1, shuffle=False
+                verbose=0, shuffle=False, epochs=2,
+                batch_size=64
                 # callbacks=[self.tensorboard]
         )
-        # print("Fit done")
+        print(f"Train done. Loss: {history.history['loss'][-1]:>2.4f}, Accuracy: {history.history['accuracy'][-1]:>2.4f}")
         if terminal_state:
             self.target_update_counter += 1
 
@@ -172,7 +177,11 @@ class DQNAgent:
         self.save_model()
 
 
-env = gym.make("MountainCar-v0")
+ENVS = []
+
+for x in range(SIM_COUNT):
+    ENVS.append(gym.make("MountainCar-v0"))
+
 action_space = 3
 obs_space = (2, )
 
@@ -185,70 +194,110 @@ agent = DQNAgent(
 
 eps_iter = iter(np.linspace(INITIAL_EPS, END_EPS, EPS_END_AT))
 
-x_graph = []
-y_graph = []
-eps_graph = []
+# x_graph = []
+# y_graph = []
+# eps_graph = []
+
+
 for epoch in range(EPOCHS):
-    done = False
-    cost = 0
-    try:
-        eps = next(eps_iter)
-    except StopIteration:
-        eps_iter = iter(np.linspace(INITIAL_EPS, END_EPS, EPS_END_AT))
-        eps = 0
-    old_state = env.reset()
+    Cost = [0] * SIM_COUNT
+    full_cost = 0
+    Envs = ENVS.copy()
+
+    New_states = []
+    for env in Envs:
+        New_states.append(env.reset())
+    New_states = np.array(New_states)
 
     if not epoch % TRAIN_EVERY:
         agent.train(True)
 
     if not epoch % SHOW_EVERY:
         render = True
+        eps = 0
     else:
         render = False
+        try:
+            eps = next(eps_iter)
+        except StopIteration:
+            eps_iter = iter(np.linspace(INITIAL_EPS, END_EPS, EPS_END_AT))
+            eps = 0
 
-    while not done:
-
+    while True:
         # Preparated Actions
         # if not x % interval:
         #     flag ^= True
         #     interval += interval + 7 + interval // 7
         #     print("Flip", f"next interval: {interval}")
         # action = 0 if flag else 2
+        Done = [False] * len(Envs)
+
+        Old_states = np.array(New_states)
+
+        New_states = []
+
         if np.random.random() < eps:
-            action = np.random.randint(0, action_space)
+            Actions = [np.random.randint(0, action_space)] * len(Envs)
         else:
-            _pred = np.array(old_state).reshape(1, -1)
-            predictions = agent.model.predict(_pred)
-            action = np.argmax(predictions)
+            Old_states = np.array(Old_states).reshape(-1, 2)
+            Predictions = agent.model.predict(Old_states)
+            Actions = np.argmax(Predictions, axis=1)
 
-        new_state, reward, done, _ = env.step(action)
-        new_transition = (old_state, action, new_state, reward, done)
-        agent.update_replay_memory(new_transition)
+        for index, env in enumerate(Envs):
+            new_state, reward, done, _ = env.step(Actions[index])
+            if abs(new_state[1]) > 0.001:
+                reward += 0.1
 
-        cost += reward
-        if render:
-            env.render()
-            time.sleep(0.001)
+            if abs(new_state[1]) > 0.003:
+                reward += 0.3
 
-        if done:
+            reward -= 0.1
+            reward -= 0.3
+
+            # else:
+            #     print(new_state)
+
+            new_transition = (Old_states[index], Actions[index], new_state, reward, done)
+            agent.update_replay_memory(new_transition)
+
+            Cost[index] += reward
+            Done[index] = done
+            New_states.append(new_state)
+
+            if render and index == 0:
+                env.render()
+                time.sleep(0.001)
+
+        for ind_d in range(len(Envs)-1, -1, -1):
+            # print(ind_d)
+            if Done[ind_d]:
+                if ind_d == 0:
+                    Envs[0].close()
+                full_cost += Cost[ind_d]
+                Cost.pop(ind_d)
+                Envs.pop(ind_d)
+                New_states.pop(ind_d)
+
+        if len(Envs) <= 0:
             break
 
-        old_state = new_state
-    env.close()
-    x_graph.append(epoch)
-    y_graph.append(cost)
-    eps_graph.append(eps)
-    print(f"Epoch {epoch:^3} finished with cost: {cost:^9}, eps: {eps:^5}")
 
-plt.figure(figsize=(16, 9))
-plt.subplot(211)
-plt.plot(x_graph, y_graph, label="Cost")
+    # x_graph.append(epoch)
+    # y_graph.append(cost)
+    # eps_graph.append(eps)
 
-plt.subplot(212)
-plt.plot(x_graph, eps_graph, label="Epsilon")
+    print(f"Epoch {epoch:^3} finished with cost_avg: {full_cost/SIM_COUNT:>3.2f}, eps: {eps:^5.3f}")
+    print(" = ="*10)
 
-plt.suptitle(f"{agent.name}")
-plt.savefig(f"{agent.name}.png")
-plt.show()
+# plt.figure(figsize=(16, 9))
+# plt.subplot(211)
+# plt.plot(x_graph, y_graph, label="Cost")
+#
+# plt.subplot(212)
+# plt.plot(x_graph, eps_graph, label="Epsilon")
+#
+# plt.suptitle(f"{agent.name}")
+# plt.savefig(f"{agent.name}.png")
+# plt.show()
 print("Session closed.")
 
